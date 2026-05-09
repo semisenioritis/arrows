7b732841642da377dd66b4bd0bf651fb2cb0561e
Atharva Dhamankar <89476245+semisenioritis@users.noreply.github.com>
2026-05-10 00:43:39 +0530

    feat: added a command to add an arrow with a random color at cursor location.

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ export default class ArrowsPlugin extends Plugin {
 	extensions: Extension[];
 	userDefinedColorsDict: {[colorName: string]: string};
 
+	private arrowStarted = false;
+	private currArrowName = "";
+
 	async onload() {
 		await this.loadSettings();
 		this.addSettingTab(new ArrowsSettingTab(this.app, this));
@@ -19,13 +22,59 @@ export default class ArrowsPlugin extends Plugin {
 			arrowsViewPlugin.extension
 		];
 		this.registerEditorExtension(this.extensions);
+
+		this.addCommand({
+			id: 'insert-arrow-at-cursor',
+			name: 'Insert arrow at cursor location',
+			editorCallback: (editor) => {
+				let arrowString = this.arrowInsertConstructor()
+				const cursor = editor.getCursor();
+				editor.replaceRange(arrowString, cursor);
+			}
+		});
 	}
 
 	onunload() {
 		const leaderLineDefs = document.getElementById("leader-line-defs");
 		if (leaderLineDefs) leaderLineDefs.remove();
 	}
+	
+	arrowInsertConstructor() {
+		if (this.arrowStarted == false){
+			const randColor = this.getRandomPastel();
+			const currTimestamp = Date.now().toString();
+			this.arrowStarted = true
+			this.currArrowName = currTimestamp
+			
+			return `{${currTimestamp}|${randColor}}`;
+		} else {
+			this.arrowStarted = false
+			return `{${this.currArrowName}}`;
+		}
 
+	}
+
+	getRandomPastel(): string {
+		const softColors = [
+			"#6f8fae",
+			"#7b6fae",
+			"#6faea3",
+			"#aea36f",
+			"#ae7f6f",
+			"#6fae7f",
+			"#6f9fae",
+			"#a06fae",
+			"#9fae6f",
+			"#ae8f6f",
+			"#6f87ae",
+			"#8f8f8f"  
+		];
+
+		return softColors[
+			Math.floor(Math.random() * softColors.length)
+		];
+	}
+	
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
